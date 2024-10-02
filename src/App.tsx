4d645820a21585645f65d5975ee6f2e9184149d4
JordiLara <jordilarareyes@gmail.com>
2024-10-02 21:44:07 +0200

    add images to onBoard messages, deleted some default code and images from root installation

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,17 @@ const App: React.FC = () => {
     {
       title: 'Dedica molestes hores',
       description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicarl-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
+      image: '/images/time_managment.svg',
     },
     {
       title: 'Programa projectes propis',
       description: 'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en un projecte ajudarà a accelerar el teu aprenentatge.',
+      image: '/images/programming.svg',
     },
     {
       title: 'Procura descansar',
       description: 'Descansar bé i desconectar són vitals. D´aquesta manera reduiràs l´estres i l´ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge',
+      image: '/images/meditation.svg',
     },
     // Agregar aquí más opciones si fuera necesario.
   ];
@@ -28,6 +31,7 @@ const App: React.FC = () => {
     <div className="App">
       <Card title={tutorialData[currentStep].title}
             description={tutorialData[currentStep].description}
+            image={tutorialData[currentStep].image}
             nextStep={nextStep} />
     </div>
   );
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,12 +1,15 @@
 interface CardProps {
     title: string;
     description: string;
+    image: string;
     nextStep: () => void;
+
   }
   
-  const Card: React.FC<CardProps> = ({ title, description, nextStep }) => {
+  const Card: React.FC<CardProps> = ({ title, description, image, nextStep }) => {
     return (
       <div className="card">
+        <img src={image} alt={title} />
         <h2>{title}</h2>
         <p>{description}</p>
         <button onClick={nextStep}>Siguiente</button>
