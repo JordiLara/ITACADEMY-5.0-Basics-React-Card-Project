63fb15461a0fdbd87828740c3248ac8e0929a125
JordiLara <jordilarareyes@gmail.com>
2024-10-05 16:52:04 +0200

    add first styles and add changes to slideshow behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 import Card from './Card';
-import './App.css';
 
 // Hook useState
 
 const App: React.FunctionComponent = () => {
+
   const [currentStep, setCurrentStep] = useState(0);
 
-
-// To-DO, añadir el background color para cada carta
 
   const tutorialData = [
     {
@@ -16,33 +14,37 @@ const App: React.FunctionComponent = () => {
       description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicarl-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
       alt: 'Una noia mira un rellotge de sorra',
       image: '/images/time_managment.svg',
+      bgColor: '#4da2a9',
     },
     {
       title: 'Programa projectes propis',
       description: 'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en un projecte ajudarà a accelerar el teu aprenentatge.',
-      alt: 'Un noi treballa a l´ordinador',
+      alt: "Un noi treballa a l'ordinador",
       image: '/images/programming.svg',
+      bgColor: '#d3d4d9',
     },
     {
       title: 'Procura descansar',
-      description: 'Descansar bé i desconectar són vitals. D´aquesta manera reduiràs l´estres i l´ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge',
+      description: "Descansar bé i desconectar són vitals. D'aquesta manera reduiràs l'estres i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge",
       alt: 'Una noia fa meditació',
       image: '/images/meditation.svg',
+      bgColor: '#ffd167',
     },
     // Agregar aquí más opciones si fuera necesario.
   ];
 
   const nextStep = () => {
-    setCurrentStep (prevStep => prevStep + 1);
+    setCurrentStep((prevStep) => (prevStep + 1) % tutorialData.length); // Reiniciar al llegar al final
   };
 
   return (
 
-    <div className = "flex-auto App">
+    <div className = "carousel w-full  flex justify-center items-center App">
         <Card title = {tutorialData[currentStep].title}
             description = {tutorialData[currentStep].description}
             image = {tutorialData[currentStep].image}
             alt = {tutorialData[currentStep].alt}
+            bgColor = {tutorialData[currentStep].bgColor}
             nextStep = {nextStep} />
       </div>
   );
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,10 +1,10 @@
 interface CardProps {
-  //TypeScript
   title: string;
   description: string;
   image: string;
   alt: string;
-  nextStep: () => void; // onClick
+  bgColor: string;
+  nextStep: () => void;
 }
 
 const Card: React.FunctionComponent<CardProps> = ({
@@ -12,14 +12,35 @@ const Card: React.FunctionComponent<CardProps> = ({
   description,
   image,
   alt,
+  bgColor,
   nextStep,
 }) => {
   return (
-    <div className="card">
-      <img src={image} alt={alt} />
-      <h2> {title} </h2>
-      <p>{description}</p>
-      <button onClick={nextStep}> ➔ </button>
+    <div
+      className = {`w-full max-w-xs md:max-w-sm h-auto md:h-[550px] rounded-3xl overflow-hidden shadow-lg flex flex-col justify-between relative`}
+      style={{ backgroundColor: bgColor }}
+    >
+      {/* Imagen superior */}
+
+      <div className = "w-full h-1/2 md:h-2/3 flex justify-center items-center">
+        <img src = {image} alt = {alt} className = "w-full h-full object-contain p-4" />
+      </div>
+
+      {/* Sección de contenido */}
+
+      <div className = "bg-white flex-grow p-6">
+        <h2 className = "font-bold text-xl mb-4 text-left">{title}</h2>
+        <p className = "text-left leading-relaxed">{description}</p>
+      </div>
+
+      {/* Botón redondo*/}
+
+      <button
+        onClick={nextStep}
+        className = "absolute bottom-4 right-4 md:bottom-6 md:right-6 w-10 h-10 bg-black text-white flex items-center justify-center rounded-full"
+      >
+        ➔
+      </button>
     </div>
   );
 };
